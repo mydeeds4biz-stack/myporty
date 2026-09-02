59bbb49fd8556d377714f404c8b3d1e79ec446e1
mydeeds4biz-stack <mydeeds4biz@gmail.com>
2026-09-02 14:47:57 +0100

    initial commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi there I am <span className="purple">Moses Andikan Best</span>{" "}
+            from <span className="purple">Lagos, Nigeria</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            I am currently working as an{" "}
+            <span className="purple">Artificial Intelligence Engineer</span> at{" "}
+            <span className="purple">Bamservices</span>.
+            <br />I hold a Bachelor of Science (Bsc.Ed) in{" "}
+            <span className="purple">Computer Education</span> from{" "}
+            <span className="purple">University of Nigeria, Nsukka</span>.
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
@@ -38,7 +38,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Moses Andikan Best</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,8 +29,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                I AM
+                <strong className="main-name"> MOSES ANDIKAN BEST</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -61,7 +61,7 @@ function Home() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/mydeeds4biz-stack"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -71,7 +71,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://x.com/Deeds4xsf"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -81,7 +81,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/andikan-b-594299113/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -91,7 +91,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/du4acell"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
